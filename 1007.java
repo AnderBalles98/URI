6a3f94efbf660eaaa80b0aeaf07f2f9823dfbf1b
AnderBalles98 <44950106+AnderBalles98@users.noreply.github.com>
2018-11-26 18:05:55 -0500

    Create 1007.java

---
+++ 1007.java
@@ -0,0 +1,22 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author ANDERSON BALLESTEROS
+ */
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
+        while (leer.ready()) {
+            int A = Integer.valueOf(leer.readLine());
+            int B = Integer.valueOf(leer.readLine());
+            int C = Integer.valueOf(leer.readLine());
+            int D = Integer.valueOf(leer.readLine());
+            System.out.println("DIFERENCA = "+((A*B)-(C*D)));
+        }
+    }
+}
